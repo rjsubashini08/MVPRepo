06f8ac359f31a9d0a02225002623eeca07310a8d
Sony <Sony@DESKTOP-GLJ4AI4>
2022-01-12 08:22:33 +0530

    commit the code

--- BesantJavaSelBatch/src/demoSelenium/OpenApplication.java
+++ BesantJavaSelBatch/src/demoSelenium/OpenApplication.java
@@ -2,6 +2,8 @@ package demoSelenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class OpenApplication {
 
@@ -9,8 +11,16 @@ public class OpenApplication {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sony\\eclipse-workspace\\BesantJavaSelBatch\\Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sony\\git\\repository\\BesantJavaSelBatch\\Drivers\\chromedriver.exe");
 		driver=new ChromeDriver();//Launch the Chrome Browser
+		
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\Sony\\git\\repository\\BesantJavaSelBatch\\Drivers\\IEDriverServer.exe");
+		driver=new InternetExplorerDriver();//Launch the Chrome Browser
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Sony\\git\\repository\\BesantJavaSelBatch\\Drivers\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		
+		
 		Thread.sleep(3000);
 		driver.manage().window().maximize();//Maximise the Browser
 		Thread.sleep(3000);
